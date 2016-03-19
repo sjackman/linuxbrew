f7343067c10bc26c67e4fcf2414407819f27d943
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-03-19 07:15:28 +0000

    hello: update 2.10 bottle for Linuxbrew.

--- Library/Formula/hello.rb
+++ Library/Formula/hello.rb
@@ -5,12 +5,10 @@ class Hello < Formula
   sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "54ac46b692fcca2abe77aeeca41976172a2dc24a6762f84a47fabcaba336df89" => :el_capitan
-    sha256 "f81a305402e8f8b6cf11a17dac81f604b6f48d940909886a6733cf4f6a64c05f" => :yosemite
-    sha256 "c80495cb6d1ad8f2c3a64c22c9dcee9d0117ca25fa6426f20a6acca275cd6c56" => :mavericks
-    sha256 "c3468e676f2c9cb511e537774424299342ffd52740e252e515bddae6d9e79df3" => :mountain_lion
-    sha256 "f81d7c0a3eee9fd62ebdecf685fbc5284e40d4a61c372d554fd45719e52a43cc" => :x86_64_linux
+    prefix "/home/linuxbrew/.linuxbrew"
+    cellar "/home/linuxbrew/.linuxbrew/Cellar"
+    revision 1
+    sha256 "26b6be39155d75f6c34a193c8cfe837589a7bf08d5c00da3671d4e01e9938fac" => :x86_64_linux
   end
 
   conflicts_with "camlistore", :because => "both install `hello` binaries"
